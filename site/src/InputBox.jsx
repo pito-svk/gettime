626dbccc76c15cd18b45e60324fb2f83f4e30dd9
Peter Parada <parad.peter@gmail.com>
2017-11-03 12:59:48 +0100

    Allow change input text

--- site/src/InputBox.jsx
+++ site/src/InputBox.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import './InputBox.css'
 
 class InputBox extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = { value: 'New York' }
+  }
+
+  handleChange (event) {
+    this.setState({ value: event.target.value })
+  }
+
   render () {
     return (
       <div className="InputBox">
-        <input autoFocus value="New York"></input>
+        <input autoFocus value={this.state.value} onChange={this.handleChange}></input>
       </div>
     )
   }
