86c81a22b5789b82708b709585a382b44cba6727
Peter Parada <parad.peter@gmail.com>
2017-11-03 08:57:07 +0100

    Add TimeResult component

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import './App.css'
 import InputBox from './InputBox.jsx'
 import Footer from './Footer.jsx'
+import TimeResult from './TimeResult.jsx'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <InputBox />
-        <div className="TimeResult" />
+        <TimeResult />
         <Footer />
       </div>
     )
---
+++ site/src/TimeResult.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './TimeResult.css'
+
+class TimeResult extends Component {
+  render () {
+    return (
+      <div className="TimeResult">
+        <h1>New York has 02:55 AM</h1>
+      </div>
+    )
+  }
+}
+
+export default TimeResult
