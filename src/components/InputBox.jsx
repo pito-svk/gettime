866717da046a1a356646eb74998cdc194b2fd4b7
Peter Parada <parad.peter@gmail.com>
2017-11-08 22:26:58 +0100

    Prepare onKeyPress event handler

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,10 +1,17 @@
 import '../styles/InputBox.css'
 
+// TODO: implement event handler to correct inputted city
+function onInputKeyPress (event) {
+  if (event.key === 'Enter') {
+    const inputContent = event.target.value
+  }
+}
+
 export default React => {
   const InputBox = props => {
     return (
       <div className="InputBox">
-        <input autoFocus value={`{TODO: city}`}></input>
+        <input autoFocus defaultValue='New York' onKeyPress={onInputKeyPress}></input>
       </div>
     )
   }
