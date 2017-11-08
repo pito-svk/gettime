a082f24b4dd3a148df9c24da30b3b087524f5fdd
Peter Parada <parad.peter@gmail.com>
2017-11-08 16:43:06 +0100

    Replace dynamic values with TODO

--- site/src/InputBox.jsx
+++ site/src/InputBox.jsx
@@ -2,16 +2,10 @@ import React, { Component } from 'react'
 import './InputBox.css'
 
 class InputBox extends Component {
-  constructor(props) {
-    super(props)
-
-    this.onChange = props.onChange.bind(this)
-  }
-
   render () {
     return (
       <div className="InputBox">
-        <input autoFocus value={this.value} onChange={this.onChange}></input>
+        <input autoFocus value={`{TODO: city}`}></input>
       </div>
     )
   }
--- site/src/TimeResult.jsx
+++ site/src/TimeResult.jsx
@@ -5,7 +5,7 @@ class TimeResult extends Component {
   render () {
     return (
       <div className="TimeResult">
-        <h1>New York has 02:55 AM</h1>
+        <h1>{`{TODO: city}`} has {`{TODO: time}`}</h1>
       </div>
     )
   }
