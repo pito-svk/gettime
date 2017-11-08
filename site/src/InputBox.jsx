7f66758dbd9c77c9cddc0e6d0865159d412287be
Peter Parada <parad.peter@gmail.com>
2017-11-08 16:37:57 +0100

    Remove handleChange and state intialization from InputBox

--- site/src/InputBox.jsx
+++ site/src/InputBox.jsx
@@ -5,19 +5,13 @@ class InputBox extends Component {
   constructor(props) {
     super(props)
 
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = { value: 'New York' }
-  }
-
-  handleChange (event) {
-    this.setState({ value: event.target.value })
+    this.onChange = props.onChange.bind(this)
   }
 
   render () {
     return (
       <div className="InputBox">
-        <input autoFocus value={this.state.value} onChange={this.handleChange}></input>
+        <input autoFocus value={this.value} onChange={this.onChange}></input>
       </div>
     )
   }
