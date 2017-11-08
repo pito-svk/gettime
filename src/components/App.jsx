90964830c1d02b32948f3326d2003b9d4a4baa9b
Peter Parada <parad.peter@gmail.com>
2017-11-08 17:29:42 +0100

    Use TimeResult as pure component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import '../styles/App.css'
 import createInputBox from './InputBox.jsx'
+import createTimeResult from './TimeResult.jsx'
 import createFooter from './Footer.jsx'
-// import TimeResult from './TimeResult.jsx'
 
 import '../styles/App.css'
 
 export default React => {
   const InputBox = createInputBox(React)
+  const TimeResult = createTimeResult(React)
   const Footer = createFooter(React)
 
   const App = props => {
     return (
       <div className="App">
         <InputBox />
+        <TimeResult />
         <Footer />
       </div>
     )
