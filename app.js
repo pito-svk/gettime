3fdbab43861590d78f0501911afedc9041e9d85e
Peter Parada <parad.peter@gmail.com>
2017-11-09 18:17:05 +0100

    Calculate lag&lng from google maps api and prepare geonames api

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ app.use(express.static(path.resolve(__dirname, 'build')))
 app.get('/api/offset/:city', async (req, res) => {
   const inputCity = req.params.city
 
-  const resp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
+  const googleMapsResp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
     .then(resp => JSON.parse(resp)))
 
-  const formattedCityName = resp.results[0].address_components[0].long_name
+  const lat = googleMapsResp.results[0].geometry.location.lat
+  const lng = googleMapsResp.results[0].geometry.location.lng
+
+  const geoNamesResp = await (request.get(`http://ws.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.GEO_NAMES_USERNAME}`)
+    .then(resp => JSON.parse(resp)))
+
+  const formattedCityName = googleMapsResp.results[0].address_components[0].long_name
 
   res.json({ city: formattedCityName })
 })
