07284269199b8aa66d90f27b22f75cf2bc6d2fcf
Peter Parada <parad.peter@gmail.com>
2017-11-09 17:56:50 +0100

    Prepare formatted city with intentional pause

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const path = require('path')
+const request = require('request-promise')
 
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.get('/api/offset/:city', (req, res) => {
+function sleep (millis) {
+  return new Promise(resolve => setTimeout(resolve, millis))
+}
+
+app.get('/api/offset/:city', async (req, res) => {
   const inputCity = req.params.city
 
-  res.json({ city: inputCity })
+  const resp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
+    .then(resp => JSON.parse(resp)))
+
+  await sleep(1000)
+
+  const formattedCityName = resp.results[0].address_components[0].long_name
+
+  res.json({ city: formattedCityName })
 })
 
 app.get('*', (req, res) => {
