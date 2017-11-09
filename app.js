5fe68e7875f72e05b956b7c412dc156d9f8278d9
Peter Parada <parad.peter@gmail.com>
2017-11-09 16:47:02 +0100

    Prepare endpoint to calculate offset of the city

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'build')))
 
+app.get('api/offset/:city', (req, res) => {
+  res.json({ status: 'TODO' })
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
