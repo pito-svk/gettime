c33b239d95b66a2375e46fe61c6096e5f36569bf
Peter Parada <parad.peter@gmail.com>
2017-11-09 17:58:55 +0100

    Remove pause from api fetch

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-function sleep (millis) {
-  return new Promise(resolve => setTimeout(resolve, millis))
-}
-
 app.get('/api/offset/:city', async (req, res) => {
   const inputCity = req.params.city
 
   const resp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
     .then(resp => JSON.parse(resp)))
-
-  await sleep(1000)
 
   const formattedCityName = resp.results[0].address_components[0].long_name
 
