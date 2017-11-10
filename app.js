f7ae19eed3f7477398f86bc4cddd3e86c4ae6f49
Peter Parada <parad.peter@gmail.com>
2017-11-10 12:14:11 +0100

    Indent

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ app.get('/api/offset/:city', async (req, res) => {
     const inputCity = req.params.city
 
     const googleMapsResp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
-    .then(resp => JSON.parse(resp)))
+      .then(resp => JSON.parse(resp)))
 
     const lat = googleMapsResp.results[0].geometry.location.lat
     const lng = googleMapsResp.results[0].geometry.location.lng
 
     const geoNamesResp = await (request.get(`http://ws.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.GEO_NAMES_USERNAME}`)
-    .then(resp => JSON.parse(resp)))
+      .then(resp => JSON.parse(resp)))
 
     const timezoneName = geoNamesResp.timezoneId
 
     const cityTime = moment.utc()
-    .tz(timezoneName)
-    .format('hh:mm A')
+      .tz(timezoneName)
+      .format('hh:mm A')
 
     const formattedCityName = googleMapsResp.results[0].address_components[0].long_name
 
