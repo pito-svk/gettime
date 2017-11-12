5de79c70647ee1d1f7ae4363dcca7e1ee8424f5a
Peter Parada <parad.peter@gmail.com>
2017-11-12 11:43:52 +0000

    Move calculating timezoneId and city coordinates into separate function

--- app.js
+++ app.js
@@ -7,23 +7,37 @@ const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'build')))
 
+// TODO: check how can throw error when resp doesn't have status code 200, maybe put option: simple into request options?
+function getLatLngFromCityName (cityName) {
+  const geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${cityName}&sensor=false`
+
+  return request.get(geocodeUrl)
+    .then(resp => JSON.parse(resp))
+    .then(resp => {
+      const lat = resp.results[0].geometry.location.lat
+      const lng = resp.results[0].geometry.location.lng
+
+      return { lat, lng }
+    })
+}
+
+function getTimezoneId ({ lat, lng }) {
+  const timezoneInfoUrl = `http://ws.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.GEO_NAMES_USERNAME}`
+
+  return request.get(timezoneInfoUrl)
+    .then(resp => JSON.parse(resp))
+    .then(resp => resp.timezoneId)
+}
+
+// Send different status when error happens, this also need to be handled on React side to check if fetch does not return status code 200
 app.get('/api/offset/:city', async (req, res) => {
   try {
     const inputCity = req.params.city
-
-    const googleMapsResp = await (request.get(`http://maps.googleapis.com/maps/api/geocode/json?address=${inputCity}&sensor=false`)
-      .then(resp => JSON.parse(resp)))
-
-    const lat = googleMapsResp.results[0].geometry.location.lat
-    const lng = googleMapsResp.results[0].geometry.location.lng
-
-    const geoNamesResp = await (request.get(`http://ws.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.GEO_NAMES_USERNAME}`)
-      .then(resp => JSON.parse(resp)))
-
-    const timezoneName = geoNamesResp.timezoneId
+    const { lat, lng } = await getLatLngFromCityName(inputCity)
+    const timezoneId await getTimezoneId({ lat, lng })
 
     const cityTime = moment.utc()
-      .tz(timezoneName)
+      .tz(timezoneId)
       .format('hh:mm A')
 
     const formattedCityName = googleMapsResp.results[0].address_components[0].long_name
