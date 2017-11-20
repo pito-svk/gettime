47ddd88c60691f29b88becc9946e3ccea7e986fb
Peter Parada <parad.peter@gmail.com>
2017-11-20 09:53:38 +0100

    Indent Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ export default React => {
   const Footer = props => {
     return (
       <div id="Footer">
-        <a href="https://github.com/pito-svk/gettime">Created by Peter Parada</a>
+        <a href="https://github.com/pito-svk/gettime">
+          Created by Peter Parada
+        </a>
       </div>
     )
   }
