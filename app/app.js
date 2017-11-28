ee652d41dfe6625fa984433505fb86fd214d72a3
Peter Parada <parad.peter@gmail.com>
2017-11-28 09:37:25 +0100

    Remove redundant imports and comment

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const path = require('path')
-const moment = require('moment-timezone')
 const { compileAndHotReload } = require('./devUtils')
-const { getCityCoordinates, getTimezoneId } = require('./core/geo')
 const { getOffset } = require('./features/offset')
 
 const app = express()
@@ -13,7 +11,6 @@ if (process.env.NODE_ENV === 'production') {
   compileAndHotReload(app)
 }
 
-// Send different status when error happens, this also need to be handled on React side to check if fetch does not return status code 200
 app.get('/api/offset/:city', getOffset)
 
 module.exports = app
