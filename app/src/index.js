78d1b57056fc9188d9634ae2e33f217c32437895
Peter Parada <parad.peter@gmail.com>
2017-12-09 13:50:17 +0100

    Rename storeProvider->storeReducer and use preloadedState of redux store as const declaration

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import createApp from './components/App'
-import storeProvider from './store'
+import storeReducer from './store'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-const store = createStore(storeProvider, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const preloadedState =
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(storeReducer, preloadedState)
 
 const App = createApp(React)
 
