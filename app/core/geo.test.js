5f0d0d314eadee988ae065e1a01af74c77b911af
Peter Parada <parad.peter@gmail.com>
2017-12-17 00:24:14 +0100

    Prepare first passing tests

---
+++ app/core/geo.test.js
@@ -0,0 +1,26 @@
+const test = require('tape')
+const { getCityCoordinates } = require('./geo')
+
+test('getCityCoordinates = New York', async t => {
+  const { formattedCityName } = await getCityCoordinates('New York')
+
+  t.equal(formattedCityName, 'New York')
+
+  t.end()
+})
+
+test('getCityCoordinates = new york', async t => {
+  const { formattedCityName } = await getCityCoordinates('new york')
+
+  t.equal(formattedCityName, 'New York')
+
+  t.end()
+})
+
+// test('getCityCoordinates = newyork', async t => {
+//   const { formattedCityName } = await getCityCoordinates('newyork')
+
+//   t.equal(formattedCityName, 'New York')
+
+//   t.end()
+// })
