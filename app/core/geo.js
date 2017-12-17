09d4b7818c202b57f2938acb7ef4737cd4b1ce85
Peter Parada <parad.peter@gmail.com>
2017-12-17 16:29:17 +0100

    Use bing spellcheck for bad typed cities

--- app/core/geo.js
+++ app/core/geo.js
@@ -1,5 +1,25 @@
 const request = require('request-promise')
 
+function spellCheck (text) {
+  const url = 'https://api.cognitive.microsoft.com/bing/v7.0/spellcheck'
+
+  const requestOptions = {
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.BING_API_KEY
+    },
+    json: true,
+    qs: {
+      text,
+      mode: 'proof'
+    }
+  }
+
+  return request.post(url, requestOptions)
+    .then(resp => {
+      return resp.flaggedTokens[0].suggestions[0].suggestion
+    })
+}
+
 // TODO: check how can throw error when resp doesn't have status code 200, maybe put option: simple into request options?
 exports.getCityCoordinates = cityName => {
   const url = 'http://nominatim.openstreetmap.org/search'
@@ -22,7 +42,7 @@ exports.getCityCoordinates = cityName => {
       return item.type === 'city' ||
         item.type === 'town'
     }))
-    .then(resp => {
+    .then(async resp => {
       try {
         // TODO: Handle resp[0] for undefined values
         const { lat, lon: lng, namedetails, address } = resp[0]
@@ -42,7 +62,53 @@ exports.getCityCoordinates = cityName => {
 
         return { lat, lng, formattedCityName }
       } catch (err) {
-        return Promise.reject(err)
+        try {
+          const spellCheckedCityName = await spellCheck(cityName)
+
+          const requestOptions = {
+            headers: {
+              'Accept-Language': 'en'
+            },
+            json: true,
+            qs: {
+              q: decodeURIComponent(spellCheckedCityName),
+              format: 'jsonv2',
+              addressdetails: 1,
+              namedetails: 1
+            }
+          }
+
+          return request.get(url, requestOptions)
+            .then(resp => resp.filter(item => {
+              return item.type === 'city' ||
+                item.type === 'town'
+            }))
+            .then(resp => {
+              try {
+                // TODO: Handle resp[0] for undefined values
+                const { lat, lon: lng, namedetails, address } = resp[0]
+                const alternativeNameOfCity = namedetails.alt_name
+                const cityName = address.city || address.town
+                let formattedCityName
+
+                if (alternativeNameOfCity &&
+                    cityName &&
+                    alternativeNameOfCity.length > 6 &&
+                    alternativeNameOfCity.length <
+                    cityName.length) {
+                  formattedCityName = alternativeNameOfCity
+                } else {
+                  formattedCityName = cityName
+                }
+
+                return { lat, lng, formattedCityName }
+              } catch (err) {
+                return Promise.reject(err)
+              }
+            })
+        } catch (err) {
+          return Promise.reject(err)
+        }
       }
     })
 }
