056798148bd7699bddca6de8dd610ba82a4fffeb
Peter Parada <parad.peter@gmail.com>
2017-12-20 15:51:30 +0100

    Fallback redis auth

--- app/utils/redis.js
+++ app/utils/redis.js
@@ -4,13 +4,6 @@ Promise.promisifyAll(redis.RedisClient.prototype)
 Promise.promisifyAll(redis.Multi.prototype)
 
 exports.redisClient = () => {
-  let redisClient = redis.createClient(process.env.REDIS_URI)
-
-  const redisPassword = process.env.REDIS_PASSWORD
-
-  if (redisPassword) {
-    redisClient = redisClient.auth(redisPassword)
-  }
-
+  const redisClient = redis.createClient(process.env.REDIS_URI)
   return redisClient
 }
