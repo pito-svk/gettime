8b55c01ec88937a56e4195ee630324138301334b
Peter Parada <parad.peter@gmail.com>
2017-12-20 15:50:46 +0100

    Authorize redis client

--- app/utils/redis.js
+++ app/utils/redis.js
@@ -4,6 +4,13 @@ Promise.promisifyAll(redis.RedisClient.prototype)
 Promise.promisifyAll(redis.Multi.prototype)
 
 exports.redisClient = () => {
-  const redisClient = redis.createClient(process.env.REDIS_URI)
+  let redisClient = redis.createClient(process.env.REDIS_URI)
+
+  const redisPassword = process.env.REDIS_PASSWORD
+
+  if (redisPassword) {
+    redisClient = redisClient.auth(redisPassword)
+  }
+
   return redisClient
 }
